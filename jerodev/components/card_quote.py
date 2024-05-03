6342e385e9dfd97a9b504e4d54e2876cdd6bc408
TheHakoDrako <jeroidracko@gmail.com>
2024-05-03 00:12:48 -0500

    "Card_quote updated"

--- jerodev/components/card_quote.py
+++ jerodev/components/card_quote.py
@@ -1,12 +1,11 @@
 import reflex as rx
 import random
-
-from jerodev.data import Quote, data
+from jerodev.data import data
 
 class QuoteState(rx.State):
     current_quote: str = "Presiona el botón para obtener una cita"
     current_author: str = "Autor"
-    quotes = [(quote.description, quote.author) for quote in data.quotes]
+    quotes = [(Quote.description, Quote.author) for Quote in data.quotes]
 
     def change_quote(self):
         description, author = random.choice(self.quotes)
