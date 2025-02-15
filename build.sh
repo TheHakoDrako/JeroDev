67d0d6a362936c3d8003a55139c7ad2fa063d051
TheHakoDrako <jeroidracko@gmail.com>
2025-02-14 23:39:41 -0500

    fix: remove --no-zip option from reflex export and adjust build script for frontend deployment

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only --no-zip
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
