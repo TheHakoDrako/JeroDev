9f968c38fc2534ff4a2af372be902da7edb65d43
TheHakoDrako <jeroidracko@gmail.com>
2025-02-15 00:01:16 -0500

    refactor: update data classes to use constructors and adjust requirements

--- jerodev/data.py
+++ jerodev/data.py
@@ -1,58 +1,83 @@
-import reflex as rx
 import json
-from dataclasses import dataclass
 
-class Media(rx.Base):
-    email: str
-    cv: str 
-    github: str
-    likedin: str
+class Media:
+    def __init__(self, email, cv, github, likedin):
+        self.email = email
+        self.cv = cv
+        self.github = github
+        self.likedin = likedin
 
-class Technology(rx.Base):
-    icon: str
-    name: str
 
-class Info(rx.Base):
-    icon: str
-    title: str
-    subtitle: str 
-    description: str
-    date: str = ""
-    certificate: str = ""
-    technologies: list[Technology] = []
-    image: str = ""
-    url: str = ""
-    github: str = ""
+class Technology:
+    def __init__(self, icon, name):
+        self.icon = icon
+        self.name = name
 
-class Extra(rx.Base):
-    image: str
-    title: str
-    description: str
-    url: str
 
-class Quote(rx.Base):
-    description: str
-    author: str
+class Info:
+    def __init__(self, icon, title, subtitle, description, date="", certificate="", technologies=[], image="", url="", github=""):
+        self.icon = icon
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+        self.date = date
+        self.certificate = certificate
+        self.technologies = [Technology(**tech) for tech in technologies]
+        self.image = image
+        self.url = url
+        self.github = github
 
-class Data(rx.Base):
-    title: str
-    description: str
-    image: str
-    avatar: str
-    name: str
-    skill: str
-    location: str
-    media: Media
-    about: str
-    technologies: list[Technology]
-    experience: list[Info]
-    projects: list[Info]
-    training: list[Info]
-    extras: list[Extra]
-    quotes: list[Quote]
+
+class Extra:
+    def __init__(self, image, title, description, url):
+        self.image = image
+        self.title = title
+        self.description = description
+        self.url = url
+
+class Quote:
+    def __init__(self, description, author):
+        self.description = description
+        self.author = author
+
+
+class Data:
+    def __init__(
+        self,
+            title,
+            description,
+            image,
+            avatar,
+            name,
+            skill,
+            location,
+            media,
+            about,
+            technologies,
+            experience,
+            projects,
+            training,
+            extras,
+            quotes
+    ):
+        self.title = title
+        self.description = description
+        self.image = image
+        self.avatar = avatar
+        self.name = name
+        self.skill = skill
+        self.location = location
+        self.media = Media(**media)
+        self.about = about
+        self.technologies = [Technology(**tech) for tech in technologies]
+        self.experience = [Info(**info) for info in experience]
+        self.projects = [Info(**info) for info in projects]
+        self.training = [Info(**info) for info in training]
+        self.extras = [Extra(**info) for info in extras]
+        self.quotes = [Quote(**quote) for quote in quotes]
 
 
 with open("assets/data/data.json", encoding="utf-8") as file:
     json_data = json.load(file)
 
-data = Data.parse_obj(json_data)
+data = Data(**json_data)
