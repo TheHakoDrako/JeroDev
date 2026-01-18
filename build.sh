0384c3dffe8bf74b4d04861cd941e25e16ed12a1
TheHakoDrako <jeroidracko@gmail.com>
2026-01-18 13:48:29 -0500

    (Fix): build.sh, requirements.txt and vercel.json adjusted.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
 reflex export --frontend-only
-unzip -o frontend.zip -d public
+unzip frontend.zip -d public
 rm -f frontend.zip
+deactivate  
